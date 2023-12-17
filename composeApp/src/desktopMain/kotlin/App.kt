d58c88843ff9eefe90d0451020761883cb12a2eb
Jacky Liu <beansoft@126.com>
2023-12-17 16:06:36 +0800

    add top

--- composeApp/src/desktopMain/kotlin/App.kt
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -1,7 +1,8 @@
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,29 +12,38 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import com.jetbrains.game.Game
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun App() {
-    MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+    val game = remember { Game() }
+    val density = LocalDensity.current
+
+    Column(modifier = Modifier.background(Color(51, 153, 255)).fillMaxHeight().fillMaxWidth()) {
+        Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
             Button(onClick = {
-                greetingText = "Compose: ${Greeting().greet()}"
-                showImage = !showImage
             }) {
-                Text(greetingText)
+                Text("开始")
             }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    null
-                )
-            }
+            Text(
+                game.gameStatus,
+                modifier = Modifier.align(Alignment.CenterVertically).padding(horizontal = 16.dp),
+                color = Color.White
+            )
         }
     }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
 }
