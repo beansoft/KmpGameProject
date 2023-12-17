af6e3ef3a4519a48f7e20f02eb17b802d6fbd993
Jacky Liu <beansoft@126.com>
2023-12-17 12:51:51 +0800

    init from kmp

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,17 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "KmpGameProject") {
+    Window(onCloseRequest = ::exitApplication, title = "Asteroids",
+        state = WindowState(
+            size = DpSize(700.dp, 900.dp)
+        )
+    ) {
         App()
     }
 }
